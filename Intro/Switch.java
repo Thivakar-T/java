65708c531aa77120eaae41e65e56c45999411ca9
THIVAKAR-T <thivakardravid1508@gmail.com>
2022-03-16 12:24:06 +0530

    Switch

--- Intro/Switch.java
+++ Intro/Switch.java
@@ -1,13 +1,42 @@
 import java.util.*;
 class Switch{
     public static void main(String[] args) {
-        today="Monday";
-        switch (today) {
-            case "Sunday":
-                System.out.println("Hai");
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the day");
+        String today=scan.nextLine();
+        switch(today) {
+            case "Sunday":{
+                System.out.println("This is Sunday");
                 break;
-            default:
+            }
+            case "Monday":{
+                System.out.println("This is Monday");
+                break;
+            }
+            case "Tuesday":{
+                System.out.println("This is Tuesday");
+                break;
+            }
+            case "Wednesday":{
+                System.out.println("This is Wednesday");
+                break;
+            }
+            case "Thursday":{
+                System.out.println("This is Thursday");
+                break;
+            }
+            case "Friday":{
+                System.out.println("This is Friday");
+                break;
+            }
+            case "Saturday":{
+                System.out.println("This is Saturday");
+                break;
+            }
+
+            default: {
                 System.out.println("Exit");
+            }
         }
     }
 }
