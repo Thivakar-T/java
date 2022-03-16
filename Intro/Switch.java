fe942b019097a2a377ece353eab944f3934787f4
THIVAKAR-T <thivakardravid1508@gmail.com>
2022-03-16 11:45:18 +0530

    Switch case

---
+++ Intro/Switch.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Switch{
+    public static void main(String[] args) {
+        today="Monday";
+        switch (today) {
+            case "Sunday":
+                System.out.println("Hai");
+                break;
+            default:
+                System.out.println("Exit");
+        }
+    }
+}
